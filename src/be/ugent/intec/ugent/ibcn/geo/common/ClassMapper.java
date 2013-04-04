38b7230f7b9a762e97d585c5963209ca62c14680
Olivier Van Laere <oliviervanlaere@gmail.com>
2013-04-04 21:50:06 +0200

    Multi-threaded on the fly cluster assignment
    - added threading to the attachElements method
    - refactored the KD initialisation step to make it reusable to copy the tree
    - local copies are made for the threads to avoid locking between threads
    - performance increase (83sec to 68sec) although 4 threads, probably due to synchronised add on the classes (locking is necessary). Can perhaps be improved by sorting the elements to an order in which they should be added to the classes
    
    Signed-off-by: Olivier Van Laere <oliviervanlaere@gmail.com>

--- src/be/ugent/intec/ugent/ibcn/geo/common/ClassMapper.java
+++ src/be/ugent/intec/ugent/ibcn/geo/common/ClassMapper.java
@@ -4,11 +4,14 @@ import be.ugent.intec.ibcn.geo.common.datatypes.Coordinate;
 import be.ugent.intec.ibcn.geo.common.datatypes.DataItem;
 import be.ugent.intec.ibcn.geo.common.datatypes.GeoClass;
 import be.ugent.intec.ibcn.geo.common.datatypes.Point;
+import be.ugent.intec.ibcn.geo.features.MostFrequentlyUsedFeatureRanker;
 import edu.wlu.cs.levy.CG.KDTree;
 import edu.wlu.cs.levy.CG.KeyDuplicateException;
 import edu.wlu.cs.levy.CG.KeySizeException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.*;
 
 /**
  * Object capable of on the fly nearest-neighbour assignments to the medoids
@@ -23,6 +26,11 @@ import java.util.List;
 public class ClassMapper {
     
     /**
+     * Number of threads, for multi-threaded processing.
+     */
+    protected static final int NR_THREADS = Runtime.getRuntime().availableProcessors();
+
+    /**
      * The KD-tree used for NN-retrieval
      */
     private KDTree<Integer> kd = new KDTree<Integer>(3);
@@ -31,6 +39,23 @@ public class ClassMapper {
      * List of known classes.
      */
     private List<GeoClass> classes = new ArrayList<GeoClass>();
+    
+    /**
+     * Holds the size of the classes list.
+     */
+    private int size = 0;
+    
+    /**
+     * @return the number of classes currently used.
+     */
+    public int size() {
+        return this.size;
+    }
+    
+    /**
+     * Holds the list of medoids used to construct this mapper.
+     */
+    private List<Point> medoids;
     
     /**
      * @return the list of known classes
@@ -53,15 +78,30 @@ public class ClassMapper {
      * @param medoids List of medoids to construct the Mapper with.
      */
     public ClassMapper(List<Point> medoids) {
+        this.medoids = medoids;
+        this.kd = initTree(false);
+        this.size = this.classes.size();
+    }
+    
+    /**
+     * @param ignoreClasses if set to true, the classes will be stored in the
+     * classes list to track them. This is the expected behaviour unless we
+     * are making copies of the KD tree for multi-threaded processing.
+     * @return a KD tree with the medoids.
+     */
+    private KDTree<Integer> initTree(boolean ignoreClasses) {
+        KDTree<Integer> localTree = new KDTree<Integer>(3);
         // Init the remapped IDs (to facilitate indexing/retrieval)
         int remappedId = 0;
         // For each of the medoids
         for (Point p : medoids) {
-            // Store the medoid with a remapped ID in the List
-            classes.add(new GeoClass(remappedId, p.getId()));
+            // If the classes should be stored
+            if (!ignoreClasses)
+                // Store the medoid with a remapped ID in the List
+                classes.add(new GeoClass(remappedId, p.getId()));
             try {
                 // Insert the medoid into the KD tree with its new ID
-                kd.insert(new Coordinate(p).doubleKey(), remappedId);
+                localTree.insert(new Coordinate(p).doubleKey(), remappedId);
             } catch (KeySizeException ex) {
                 System.err.println("Error: " + ex.getMessage());
             } catch (KeyDuplicateException ex) {
@@ -70,6 +110,7 @@ public class ClassMapper {
             // Increment the remapped ID counter
             remappedId++;
         }
+        return localTree;
     }
     
     /**
@@ -79,6 +120,17 @@ public class ClassMapper {
      * no class was found.
      */
     public int findClassId(Point p) {
+        return findClassId(p, this.kd);
+    }
+    
+    /**
+     * Finds the ID of the nearest class for the given Point.
+     * @param p A Point object
+     * @param the KD tree to use for lookup
+     * @return the ID of the GeoClass that 'contains' the given Point or -1 if
+     * no class was found.
+     */
+    private int findClassId(Point p, KDTree<Integer> kd) {
         // Sanity check
         if (p != null) {
             try {
@@ -110,8 +162,19 @@ public class ClassMapper {
      * no class was found.
      */
     public GeoClass findClass(Point p) {
+        return findClass(p, this.kd);
+    }
+    
+    /**
+     * Finds the nearest class for the given Point.
+     * @param p A Point object
+     * @param the KD tree to use for lookup
+     * @return the actual GeoClass that 'contains' the given Point or null if
+     * no class was found.
+     */
+    private GeoClass findClass(Point p, KDTree<Integer> kd) {
         // Re-use the findClassId method
-        int classId = findClassId(p);
+        int classId = findClassId(p, kd);
         if (classId == -1)
             // Return -1 if not found
             return null;
@@ -126,16 +189,86 @@ public class ClassMapper {
     public void attachElements(DataItem [] data) {
         System.out.println("Fully attaching training data to GeoClasses...");
         long t1 = System.currentTimeMillis();
-        for (DataItem item : data) {
-            // Sanity check
-            if (item != null) {
-                // On the fly associate the clustering
-                GeoClass myClass = findClass(item);
-                // Add this ID to the class
-                myClass.addElement(item.getId());
+        // Prepare a thread pool
+        ExecutorService executor = Executors.newFixedThreadPool(NR_THREADS);
+        // Determine block length
+        int length = (int) (data.length * 1.0 / NR_THREADS);
+        for (int i = 0; i < NR_THREADS; i++) {
+            int begin = i * length;
+            if (i == NR_THREADS - 1) {
+                length = data.length - (i * length);
             }
+            int end = begin + length;
+            // Start helper runnables with a copy of the tree
+            executor.submit(new AttachHelper(data, begin, end, initTree(true)));
         }
+        // This will make the executor accept no new threads
+        // and finish all existing threads in the queue
+        executor.shutdown();
+        // Wait until all threads are finish
+        while (!executor.isTerminated()) {}
         long t2 = System.currentTimeMillis();
         System.out.println("Finished. Took "+(t2-t1)+" ms.");
+    }
+    
+    /**
+     * Helper Runnable for multi-threaded on the fly cluster association.
+     */
+    private class AttachHelper implements Runnable {
+
+        /**
+         * Reference to the training data.
+         */
+        private DataItem [] data;
+        
+        /**
+         * Start index for processing.
+         */
+        private int begin;
+
+        /**
+         * End index for processing.
+         */
+        private int end;
+        
+        /**
+         * Reference to the local KD tree.
+         */
+        private KDTree<Integer> tree;
+        
+        /**
+         * 
+         * @param data Training data
+         * @param begin Start index for processing.
+         * @param end End index for processing.
+         * @param tree Local copy of the KD tree
+         */
+        public AttachHelper(DataItem [] data, int begin, int end, KDTree<Integer> tree) {
+            this.data = data;
+            this.begin = begin;
+            this.end = end;
+            this.tree = tree;
+        }
+        
+        /**
+         * Actual cluster association.
+         */
+        @Override
+        public void run() {
+            for (int i = begin; i < end; i++) {
+                DataItem item = data[i];
+                // Sanity check
+                if (item != null) {
+                    // On the fly associate the clustering, provide the tree
+                    // to avoid locking on the shared tree
+                    GeoClass myClass = findClass(item, tree);
+                    // Lock on the class object
+                    synchronized(myClass) {
+                        // Add this ID to the class
+                        myClass.addElement(item.getId());
+                    }
+                }
+            }
+        }
     }
 }
